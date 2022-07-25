33a5f79763efc9a419d871a1d182118ae9171b1d
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 18:09:01 +0530

    Add files via upload

---
+++ do-some-work.js
@@ -0,0 +1,15 @@
+var qhttp = require('q-io/http');
+// Send an HTTP GET request to the session cache on port 7000.  A stringwill be returned to you representing a user ID
+qhttp.read("http://localhost:7000/")
+
+//Grab that ID from the session response and send an HTTP GET request toyour database on port 7001 to the url `localhost:7001/<id>`
+.then(function (id) {
+  return qhttp.read("http://localhost:7001/" + id);
+})
+.then(function (json) {
+  console.log(JSON.parse(json));
+})
+.then(null, console.error)
+.done();
+
+
