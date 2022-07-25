bf12d2415efda904bdb2d81b21ed47ab3a810dc2
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 15:03:50 +0530

    Update reject-a-promise.js

--- reject-a-promise.js
+++ reject-a-promise.js
@@ -1,11 +1,11 @@
 'use strict';
-
+//Create a promise that after a delay of 300ms, rejects with an Error object
 var promise = new Promise(function (fulfill, reject) {
     setTimeout(function() {
         reject(new Error('REJECTED!'));
     }, 300);
 })
-
+//print the error.message using console.log
 function onReject(error) {
     console.log(error.message);
 }
