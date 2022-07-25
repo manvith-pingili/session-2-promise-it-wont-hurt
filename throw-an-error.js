e5a6fe55fc035021e8501a3438cfa9da6aee5d61
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 18:18:08 +0530

    Update throw-an-error.js

--- throw-an-error.js
+++ throw-an-error.js
@@ -1,4 +1,8 @@
 'use strict';
+/* 
+Build a function called `parsePromised` that creates a promise,performs `JSON.parse` in a `try`/`catch` block,
+and fulfills or rejectsthe promise depending on whether an error is thrown 
+*/
 function parsePromised(json) {
   return new Promise(function (fulfill, reject) {
     try {
