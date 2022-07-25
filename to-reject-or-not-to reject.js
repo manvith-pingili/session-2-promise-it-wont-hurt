d48b0493466a925d6ce42eb780f4acceefefe629
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 15:22:32 +0530

    Update to-reject-or-not-to reject.js

--- to-reject-or-not-to reject.js
+++ to-reject-or-not-to reject.js
@@ -1,12 +1,13 @@
 'use strict';
+//create a promise and fulfill the promise with the value 'I FIRED' and reject the promise with the value 'I DID NOT FIRE'
 var promise = new Promise(function (fulfill, reject) {
     fulfill('I FIRED');
     reject(new Error('I DID NOT FIRE'));
 });
-
+//create a function that prints the error's message with console.log
 function onRejected(error){
     console.log(error.message);
 }
-
+//pass console.log and the onRejected function as the success and rejection handlers
 promise.then(console.log, onRejected);
 
