5893464e4d0f53811e34ba37c3a09aecd6e9b8ea
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 16:50:46 +0530

    Add files via upload

---
+++ an-important-rule.js
@@ -0,0 +1,22 @@
+'use strict';
+//Create a function `alwaysThrows` that throws an `Error` withtext `"OH NOES"`
+function alwaysThrows() {
+    throw new Error('OH NOES');
+}
+// Create a function `iterate` that prints the first argument(an integer) to it and then returns that argument + 1
+function iterate(num) {
+    console.log(num);
+    return num+1;
+}
+
+function onReject(error) {
+    console.log(error.message);
+}
+/*
+Create a promise chain using `Promise.resolve` that wraps your iterate method, then a series of iterations that attempts to perform `iterate` a total of 10 times.
+Attach a rejection handler at the bottom of your chain to print the`error.message` using `console.log`.
+Insert a call to `alwaysThrows` after your 5th call of `iterate`
+*/
+Promise.resolve(iterate(1)).then(iterate).then(iterate).then(iterate).then(iterate).then(alwaysThrows).then(iterate).then(iterate).then(iterate).then(iterate).then(iterate).catch(onReject);
+
+
