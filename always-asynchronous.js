05e2655488d6d651b126fea45a701f15981b0cf2
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 15:24:29 +0530

    Update always-asynchronous.js

--- always-asynchronous.js
+++ always-asynchronous.js
@@ -1,8 +1,9 @@
 'use strict';
-
+//create a promise and fulfill it with a value 'PROMISE VALUE'
 var promise = new Promise(function (fulfill, reject) {
     fulfill('PROMISE VALUE');
 });
-
+//pass console.log as the success handler
 promise.then(console.log);
+//print 'MAIN PROGRAM' using console.log
 console.log('MAIN PROGRAM');
