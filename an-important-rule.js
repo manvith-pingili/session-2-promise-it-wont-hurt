ee36eb1300ba40a9975465a8644eeb36d5d1b9d4
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 18:41:41 +0530

    Update an-important-rule.js

--- an-important-rule.js
+++ an-important-rule.js
@@ -13,9 +13,9 @@ function onReject(error) {
     console.log(error.message);
 }
 /*
-Create a promise chain using `Promise.resolve` that wraps your iterate method, then a series of iterations that attempts to perform `iterate` a total of 10 times.
-Attach a rejection handler at the bottom of your chain to print the`error.message` using `console.log`.
-Insert a call to `alwaysThrows` after your 5th call of `iterate`
+Create a promise chain using `Promise.resolve` that wraps the iterate method, then a series of iterations that attempts to perform `iterate` a total of 10 times.
+Attach a rejection handler at the bottom of the chain to print the`error.message` using `console.log`.
+Insert a call to `alwaysThrows` after the 5th call of `iterate`
 */
 Promise.resolve(iterate(1)).then(iterate).then(iterate).then(iterate).then(iterate).then(alwaysThrows).then(iterate).then(iterate).then(iterate).then(iterate).then(iterate).catch(onReject);
 
